9bec58bbafae82c57e0e48934e73cd8ab06a070f
Permadi Wibisono <mbapewe@gmail.com>
2021-11-14 02:41:24 +0700

    Add babel

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ const foo = {
   username: "permadiwibisono",
   githubURL: "https://github.com/permadiwibisono",
 };
+const foo2 = {
+  ...foo,
+  repoURL: "https://github.com/permadiwibisono/react-webpack5-starter.git",
+};
 
 console.log(foo);
+console.log(foo2);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
+module.exports = {
+  mode,
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
