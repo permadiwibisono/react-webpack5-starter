11f1e6342b70e149624d25f9453937fd3e8e8d49
Permadi Wibisono <mbapewe@gmail.com>
2023-04-26 10:59:48 +0700

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,20 @@ module.exports = {
     browser: true,
     es2021: true
   },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:prettier/recommended"
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
-  plugins: ["react"],
-  settings: {
-    react: {
-      version: "detect"
-    }
-  }
+  plugins: ["react"]
 };
